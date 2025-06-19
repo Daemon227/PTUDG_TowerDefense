007145353702c581022d6c286b9fcadc2d2bbca8
Daemon227 <Hoangtiendat220704@gmail.com>
2025-06-19 09:05:13 +0700

    update ui code

--- Assets/Scripts/CombatWorld/UI/CombatButtonManager/LevelUpTower.cs
+++ Assets/Scripts/CombatWorld/UI/CombatButtonManager/LevelUpTower.cs
@@ -1,8 +1,31 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class LevelUpTower : MonoBehaviour, IPointerClickHandler
 {
+    public Image image;
+    public Sprite state1;
+    public Sprite state2;
+    public TextMeshProUGUI coinText;
+
+    private GameObject nextLevel;
+    private void Update()
+    {
+        image.sprite = state1;
+
+        nextLevel = BuyTowerUIManager.Instance.GetCurrentObject().GetComponent<ITower>().NewTower;
+        if(nextLevel == null)
+        {
+            image.sprite = state2;
+            coinText.text = "Max";
+        }
+        else
+        {
+            coinText.text = nextLevel.GetComponent<ITower>().Cost+"";
+        }
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
         BuyTowerUIManager.Instance.LevelUpTower();
--- Assets/Scripts/CombatWorld/UI/CombatButtonManager/SellTower.cs
+++ Assets/Scripts/CombatWorld/UI/CombatButtonManager/SellTower.cs
@@ -1,8 +1,16 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SellTower : MonoBehaviour, IPointerClickHandler
 {
+    public TextMeshProUGUI coinText;
+    private void Update()
+    {
+        GameObject current = BuyTowerUIManager.Instance.GetCurrentObject();
+        if (current == null) coinText.text = "0";
+        else coinText.text = current.GetComponent<ITower>().Cost * 0.6f + "";
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
         BuyTowerUIManager.Instance.SellTower();
